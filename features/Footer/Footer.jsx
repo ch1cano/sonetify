8161c063de7b511cc3bebfd9dcd510125eb75718
ch1cano <ch1canoo358@gmail.com>
2023-07-21 16:14:25 +0300

    fix final

---
+++ features/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./Footer.module.css"
+
+const Footer = () => {
+    return <div className={s.footer}></div>
+};
+
+export default Footer;
---
+++ pages/about.tsx
@@ -0,0 +1,106 @@
+import Head from "next/head";
+import { FC } from "react";
+import s from "./About.module.css";
+import aboutPhoto from "../assets/landingPhoto/aboutPhoto.jpg";
+import Image from "next/image";
+
+const AboutPage: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Обо мне</title>
+      </Head>
+      <main className="w-full h-full d-flex">
+        {/* <div className={s.wrapper}>
+          <div className={s.block}>
+            <div className={s.bgPhoto}>
+              <Image src={aboutPhoto} className={s.aboutPhoto} alt="SONETIFY" />
+              <p className={s.bunerRightDownPhoto}>
+                <strong>РЕПРИНЕЦ СОФИЯ</strong>
+              </p>
+            </div>
+
+            <div className={s.text}>
+              <div className={s.textBuner}>
+                <strong>SONETIFY</strong>
+              </div>
+              <h1 className={s.headName}>
+                <strong>ОБРАЗОВАНИЕ</strong>
+              </h1>
+              <p className={s.headDesc}>
+                ПРЕЗИДЕНТСКАЯ АКАДЕМИЯ РАНХИГС
+                <br />
+                ФАКУЛЬТЕТ: ПСИХОЛОГИЧЕСКОЕ КОНСУЛЬТИРОВАНИЕ И КОУЧИНГ
+              </p>
+              <h1 className={s.tg}>
+                <strong>TELEGRAM</strong>
+              </h1>
+              <div className={s.tags}>
+                <a href="https://t.me/sonetify" target="blank">
+                  @SONETIFY
+                </a>
+              </div>
+              <p className={s.bunerLeftUpPhoto}>
+                <strong>
+                  ВАШ ПСИХОЛОГ
+                  <br /> ДРУГ <br /> СОВЕТНИК
+                  <br />И ПРЕДАННЫЙ СЛУШАТЕЛЬ
+                </strong>
+              </p>
+            </div>
+          </div>
+        </div> */}
+        <div className={s.wrapper}>
+          <div className={s.photo__block}>
+            <Image
+              src={aboutPhoto}
+              className={s.aboutPhoto}
+              alt="SONETIFY"
+              width={600}
+            />
+            <p className={s.img__text}>
+              <strong>РЕПРИНЕЦ СОФИЯ</strong>
+            </p>
+            <div className={s.text__who__am__i_mobile}>
+              <p>Ваш психолог</p>
+              <p>Друг советник</p>
+              <p>И преданный слушатель</p>
+            </div>
+          </div>
+
+          <div className={s.text__block}>
+            <h1>Sonetify</h1>
+            <div className={s.text__about}>
+              <div className={s.text__education}>
+                <h3>
+                  <strong>Образование</strong>
+                </h3>
+                <p>ПРЕЗИДЕНТСКАЯ АКАДЕМИЯ РАНХИГС</p>
+                <p>ФАКУЛЬТЕТ: ПСИХОЛОГИЧЕСКОЕ КОНСУЛЬТИРОВАНИЕ И КОУЧИНГ</p>
+              </div>
+
+              <div className={s.text__telegram}>
+                <h3>
+                  <strong>Telegram</strong>
+                </h3>
+                <p>
+                  <a href="https://t.me/sonetify" target="blank">
+                    @SONETIFY
+                  </a>
+                </p>
+              </div>
+              
+            </div>
+            <div className={s.text__who__am__i}>
+              <p>Ваш психолог</p>
+              <p>Друг советник</p>
+              <p>И преданный слушатель</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default AboutPage;
