321409620ee1de50a6722d624ceb1425efba1f67
lakeTP <shmelkov15@bk.ru>
2023-09-16 14:21:53 +0300

    next try to fix img res

--- pages/form/form.tsx
+++ pages/form/form.tsx
@@ -54,11 +54,7 @@ const Form = () => {
 					</p>
 				</div>
 				<div className={s.inputName}>Имя:</div>
-				<input
-					className={s.email}
-					type="name"
-					{...register("name", { required: true })}
-				/>
+
 				<div className={s.inputName}>Email:</div>
 				<input
 					className={s.email}
